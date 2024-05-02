2a515369e15356cfef603df3984d830fda2b14cd
minisbett <39670899+minisbett@users.noreply.github.com>
2024-05-02 22:52:50 +0200

    Add comments

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/RhythmEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/RhythmEvaluator.cs
@@ -9,17 +9,26 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
 {
     public static class RhythmEvaluator
     {
+        /// <summary>
+        /// The amount of past hit objects (including the current) to consider.
+        /// </summary>
         private const int maxobjectcount = 20;
+
+        /// <summary>
+        /// The amount of iterations for the coefficient.
+        /// </summary>
         private const int coefiterations = 8;
 
         public static double EvaluateDifficultyOf(DifficultyHitObject current)
         {
+            // Get the delta times for the past hit objects.
             double[] deltaTimes = Enumerable.Range(0, maxobjectcount)
                 .TakeWhile(x => current.Previous(x - 1) is not null)
                 .Select(x => current.Previous(x - 1).DeltaTime).ToArray();
 
             double entropy = 0;
 
+            // Calculate the probability of occurrence for each delta time in the past window of delta times and adjust the entropy.
             foreach (double x in deltaTimes)
             {
                 double probability = p(x, deltaTimes);
@@ -27,11 +36,14 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
                 entropy += -probability * Math.Log(probability);
             }
 
+            Console.WriteLine($"Entropy for {current.Index} is {entropy}");
+
             return entropy;
         }
 
         private static double p(double x, double[] deltaTimes)
         {
+            // Calculates the coefficient for the rhythmic difference between two delta times.
             double coef(double x, double i)
                 => Enumerable.Range(1, coefiterations)
                      .Sum(n => Math.Pow(Math.Cos(x / i * n * Math.PI), 2) / biggestPrimeFactor[n])
@@ -39,13 +51,17 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
 
             double probability = 0;
 
+            // Calculate the probability of occurrence for a delta time x in the past window of delta times.
             foreach (double i in deltaTimes)
                 probability += coef(x, i);
 
+            // Get the average probability.
             return probability / deltaTimes.Length;
         }
 
-
+        /// <summary>
+        /// An array with the biggest prime factor of the index.
+        /// </summary>
         private static int[] biggestPrimeFactor = new int[]
         {
             0,
@@ -56,7 +72,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
             5,
             3,
             7,
-            2
+            2,
         };
     }
 }
